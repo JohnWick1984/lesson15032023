c24cbed0ac2293870be05bbbdaec593cfdf5790b
strupeiq <alinka-roxy@mail.ru>
2023-03-17 21:29:32 +0500

    Добавьте файлы проекта.

---
+++ 17.03.2023/17.03.2023.cpp
@@ -0,0 +1,30 @@
+﻿// 17.03.2023.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<Windows.h>
+#include<cstdio>
+#include<fstream>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+
+
+int main()
+{
+    setlocale(0, "");
+
+    ofstream out;
+     out.open("C:\\Users\\ST\\Documents\\Шумаков\\lesson15032023\\17.03.2023\\123.txt");
+
+     if (out.is_open())
+     {
+         out << "Hello world!" << "\n";
+
+     } 
+
+     cout << "End of program" << "\n";
+     return 0;
+
+}
+
---
+++ lesson15032023/lesson15032023.cpp
@@ -0,0 +1,33 @@
+﻿// lesson15032023.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<Windows.h>
+using namespace std;
+
+union Employee
+{
+	int id;
+	double salary;
+
+};
+
+int main()
+{
+	setlocale(0, "");
+
+
+	Employee myEmployee;
+
+	myEmployee.id = 15;
+	cout << "The first value (id) at ";
+	cout << "the allocated memory: ";
+	cout << myEmployee.id << "\n";
+	myEmployee.salary = 100.35;
+	cout << " The next value (salary) stored ";
+	cout << "after removint the previouse value: ";
+	cout << myEmployee.salary << "\n";
+
+
+}
+
